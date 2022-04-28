20b08f79b23b4ccf15c18d4d86be622a6ed4f5df
Muskan <muskan26sayyad@gmail.com>
2022-04-28 21:22:32 +0530

    counter demo first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CounterApp';
+
+  countNumber = 0;
+
+  resetBtn()
+  {
+    this.countNumber = 0;
+  }
+
+  //increaceBtn
+  increaseBtn()
+  {
+    this.countNumber = this.countNumber + 1;
+  }
+
+  //decreasingBtn
+  decreaseBtn()
+  {
+    this.countNumber = this.countNumber - 1;
+    if(this.countNumber<0){
+      this.countNumber = 0;
+    }
+  }
 }
